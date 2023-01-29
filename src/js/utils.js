5f0f916acb21d322ef5ec3869ae7dad33cc259c0
Anthony K <kilospade@gmail.com>
2023-01-29 16:50:00 -0600

    add approx. read time

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,7 @@
 export function randomElementFromArray(arr) {
   return arr[(arr.length * Math.random()) | 0];
 }
+
+export function roundAccurate(number, decimals) {
+  return Number(Math.round(number + "e" + decimals) + "e-" + decimals);
+}
--- src/jsx/SpeedReader/display.jsx
+++ src/jsx/SpeedReader/display.jsx
@@ -1,17 +1,35 @@
 import { Match, Switch, useContext } from "solid-js";
+import { roundAccurate } from "../../js/utils";
 import { SRContext, STATES } from "./context";
 
 import styles from "./index.module.css";
 
 export default function Display(props) {
   const [state] = useContext(SRContext);
+
+  function getEstimate() {
+    let len = (state.input.length * state.speed) / 1000,
+      unit = len > 60 ? "minute" : "second";
+    if (unit === "minute") len /= 60;
+    return `${state.input.length} words at ${
+      state.speed
+    }ms approx. ${roundAccurate(len, 2)} ${unit}${len > 1 ? "s" : ""}`;
+  }
+
   return (
     <div class={styles.display}>
       <Switch>
-        <Match when={state.state === STATES.IDLE}>Waiting to start!</Match>
-        <Match when={state.state === STATES.STARTING}>Get ready!</Match>
+        <Match when={state.state === STATES.IDLE}>
+          <div class={styles.centerText}>
+            <p>Waiting to start!</p>
+            <p class={styles.small}>{getEstimate}</p>
+          </div>
+        </Match>
+        <Match when={state.state === STATES.STARTING}>
+          <p>Get ready!</p>
+        </Match>
         <Match when={state.state === STATES.RUNNING}>
-          {state.input[state.current]}
+          <p>{state.input[state.current]}</p>
         </Match>
       </Switch>
     </div>
